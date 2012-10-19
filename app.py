ec4de6536ee1fcca26bf8da5d4b2fd59f8848ac7
Josh Fernandes <joshfernandes@mac.com>
2012-10-18 18:06:16 -0700

    tried html

--- app.py
+++ app.py
@@ -1,11 +1,12 @@
 import os
 from flask import Flask
 
-app = Flask(__name__)
-
-@app.route('/')
-def hello():
-    return 'Hello World!'
+print '<HTML><HEAD>'
+print '<TITLE>Hello World</TITLE></HEAD>'
+print '<BODY><H1>Hello World</H1>'
+print '<P>Program with'
+print '<A HREF="http://www.python.org/"'
+print '>Python</A> today</BODY></HTML>'
 
 if __name__ == '__main__':
     # Bind to PORT if defined, otherwise default to 5000.
