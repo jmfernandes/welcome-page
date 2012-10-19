9474b26e4023bb4f188fdfade3fa2465141ee327
Josh Fernandes <joshfernandes@mac.com>
2012-10-19 10:45:35 -0700

    trying to incorporate css

--- app.py
+++ app.py
@@ -9,6 +9,9 @@ from flask import render_template
 def hello(name=None):
     return render_template('hello.html', name=name)
 
+with app.test_request_context():
+ print url_for('static', filename='mystyle.css')
+
 
 
 if __name__ == '__main__':
