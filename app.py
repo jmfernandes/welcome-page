cc03b854c6180c7e889ff6017c8a15b800220daa
Josh Fernandes <joshfernandes@mac.com>
2012-11-01 15:16:24 -0700

    whoo

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 import os
 import json
-from flask import Flask, Render_template, Url_for
+from flask import Flask, render_template, url_for
 from werkzeug.routing import Map, Rule, NotFound, RequestRedirect, BaseConverter
 
 app = Flask(__name__)
